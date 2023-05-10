a5bbbc1cd2ae8427c28aa55aaf47c827e49058a3
Sheyy <kelvinhuynhalves1102@gmail.com>
2023-05-10 16:04:46 +0700

    T2FE03 feat: Building UI for register page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./Components/Layouts/MainLayout";
 import Login from "./Components/Pages/Login/Login";
+import Register from "./Components/Pages/Register/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<MainLayout children={<Login />} />}  />
+      <Route path="/register" element={<MainLayout children={<Register />} />}  />
     </Routes>
   );
 }
