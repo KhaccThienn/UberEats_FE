74aa362174794c3850efe402077aa39cc55116f6
Sheyy <105706186+sheyy3103@users.noreply.github.com>
2023-05-11 16:48:25 +0700

    T2 fe05 feat building UI for home page client (#6)
    Co-authored-by: Lê Khắc Thiện <muzankibut112@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "./Components/Layouts/MainLayout";
 import Login from "./Components/Pages/Login/Login";
 import Register from "./Components/Pages/Register/Register";
+import Home from "./Components/Pages/Client/Home/Home";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<MainLayout children={<Home />} />}  />
       <Route path="/login" element={<MainLayout children={<Login />} />}  />
       <Route path="/register" element={<MainLayout children={<Register />} />}  />
     </Routes>
