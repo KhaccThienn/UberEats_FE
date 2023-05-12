b08533bb7234d3074030dba8b2231efe0b613de1
Sheyy <kelvinhuynhalves1102@gmail.com>
2023-05-12 10:29:55 +0700

    T2FE10 feat building 404 page

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import Login from "./Components/Pages/Login/Login";
 import Register from "./Components/Pages/Register/Register";
 import jwt_decode from "jwt-decode";
 import Home from "./Components/Pages/Client/Home/Home";
+import Error from "./Components/Pages/Error/Error";
 
 function App() {
-  const getToken = () => {
-    const token = localStorage.getItem("access_token");
-    return token.toString();
-  };
+  // const getToken = () => {
+  //   const token = localStorage.getItem("access_token");
+  //   return token.toString();
+  // };
 
-  const decoded = jwt_decode(getToken());
-  
-  console.log(decoded);
+  // const decoded = jwt_decode(getToken());
+
+  // console.log(decoded);
   // idea:
   // const clientRoute = [
   //   {
@@ -25,9 +26,10 @@ function App() {
   // ];
   return (
     <Routes>
-      <Route path="/" element={<MainLayout children={<Home />} />}  />
-      <Route path="/login" element={<MainLayout children={<Login />} />}  />
-      <Route path="/register" element={<MainLayout children={<Register />} />}  />
+      <Route path="/" element={<MainLayout children={<Home />} />} />
+      <Route path="/login" element={<MainLayout children={<Login />} />} />
+      <Route path="/register" element={<MainLayout children={<Register />} />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
