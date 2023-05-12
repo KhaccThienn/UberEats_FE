0c419e9b6f9371d233d659420c7a093ac107f977
Sheyy <105706186+sheyy3103@users.noreply.github.com>
2023-05-12 16:03:53 +0700

    T2 fe05 feat building UI for home page client (#9)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "./Components/Layouts/MainLayout";
 import Login from "./Components/Pages/Login/Login";
 import Register from "./Components/Pages/Register/Register";
-import jwt_decode from "jwt-decode";
 import Home from "./Components/Pages/Client/Home/Home";
 import Error from "./Components/Pages/Error/Error";
 
@@ -26,6 +25,7 @@ function App() {
   // ];
   return (
     <Routes>
+
       <Route path="/" element={<MainLayout children={<Home />} />} />
       <Route path="/login" element={<MainLayout children={<Login />} />} />
       <Route path="/register" element={<MainLayout children={<Register />} />} />
